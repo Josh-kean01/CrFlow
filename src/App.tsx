b159288a5fc759122b82240b4755a9ba4329be47
Joshkean01 <adekunlej960@gmail.com>
2026-05-14 11:00:07 +0100

    Switched from brwoser router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import NotificationCenter from './components/NotificationCenter';
@@ -17,7 +17,7 @@ export default function App() {
   const [notificationCenterOpen, setNotificationCenterOpen] = useState(false);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="flex min-h-screen bg-background overflow-visible">
         <Sidebar />
         <div className="flex-1 flex flex-col min-w-0 overflow-visible">
@@ -39,6 +39,6 @@ export default function App() {
         isOpen={notificationCenterOpen}
         onClose={() => setNotificationCenterOpen(false)}
       />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
